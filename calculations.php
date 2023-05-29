fea4a2574402a51a16acfcfa6a3c562de11614bd
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-05-29 15:48:57 +0000

    Not working but code is corrected

--- calculations.php
+++ calculations.php
@@ -1,13 +1,11 @@
 <?php
 
 // Setting variables.
-function factorsSent() {
   $counter = 1;
   $numberString = "";
   $message = "";
   
   // Initializing variables and constant.
-  $reverseYes = $_POST["reverse-yes"];
   $number = intval($_POST["number"]);
   $absNumber = abs($number);
 
@@ -21,31 +19,6 @@ function factorsSent() {
     $message = "Zero has no factors.";
   }
 
-  // Otherwise, if the reversed option is selected, display the user's factors in reverse order.
-  else if ($reverseYes) {
-    $counter = -$number;
-    $message = "Here is a list of all the factors for your number:<br><br>";
-    
-    // For loop to find factors in reverse order.
-    for ($counter = -$absNumber; $counter <= $absNumber; $counter++) {
-      if ($absNumber % $counter == 0) {
-        $numberString .= $counter . "<br>";
-      }
-    }
-
-    // If the number is divided by 2 and has a remainder of 0, then display "The number you entered is even."
-    if ($number % 2 == 0) {
-      $message .= "The number you entered is even.<br>";
-    } 
-    // Else, display "The number you entered is odd."
-    else {
-      $message .= "The number you entered is odd.<br>";
-    }
-
-    // Add the factor list to the message.
-    $message .= $numberString;
-
-  }
   // Else (reversed option not selected)
   else {
     $counter = -$number;
@@ -73,5 +46,5 @@ function factorsSent() {
   }
 
   echo $message;
-}
+
 ?>
